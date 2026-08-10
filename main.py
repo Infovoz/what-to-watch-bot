073f5a82bb67b0bb1d48de71a59baf91a6bff9dc
Infovoz <gogg8170@gmail.com>
2026-08-11 01:39:27 +0300

    Добавлен init_db() при запуске

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from handlers.routes import router
 from config import BOT_TOKEN
+from database.models import init_db
 
 dp = Dispatcher()
 dp.include_router(router)
 
 
 async def main():
+    await init_db()
+    print("✅ База данных готова")
+
     bot = Bot(token=BOT_TOKEN)
+    print("🤖 Бот запущен!")
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
-
